99477e3a7baac9122c8e2b49fb43034a9a5af1e0
Trevor <tlarnold10@hotmail.com>
2020-05-18 10:22:57 -0500

    cleaned up the html a little bit and nav headers

--- static/site/workout_chart.js
+++ static/site/workout_chart.js
@@ -7,7 +7,7 @@ function showChart(data, labels, sums) {
     html += "<table class=\"table is-fullwidth is-striped is-hoverable\>\
                 <thead class=\"thead\">\
                 <th class=\"th\">Date</th>\
-                <th class=\"th\">Duration</th>\
+                <th class=\"th\">Duration (minutes)</th>\
                 <th class=\"th\">Type</th>\
                 </thead>";
     for (i=0; i<data.length; i++) {
@@ -25,7 +25,7 @@ function showChart(data, labels, sums) {
     html += "<table class=\"table is-fullwidth is-striped is-hoverable\>\
                 <thead class=\"thead\">\
                 <th class=\"th\">Workout</th>\
-                <th class=\"th\">Total Duration</th>\
+                <th class=\"th\">Total Duration (minutes)</th>\
                 </thead>";
     for (i=0; i<labels.length; i++) {
         html += "<tr class=\"tr\">";
